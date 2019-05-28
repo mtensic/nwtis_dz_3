283844e9abfca4d9b9dac1e24399752824e4ea68
Tom <peharda.tomislav.22@gmail.com>
2019-05-28 13:45:40 +0200

    Aplikacija 2

---
+++ mtensic_zadaca_3_2/src/java/org/foi/nwtis/mtensic/podaci/Aerodrom.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.foi.nwtis.mtensic.podaci;
+
+import org.foi.nwtis.rest.podaci.Lokacija;
+
+/**
+ *
+ * @author nwtis_3
+ */
+public class Aerodrom {
+    private String icao;
+    private String naziv;
+    private String drzava;
+    private Lokacija lokacija;
+
+    public Aerodrom() {
+    }
+
+    public Aerodrom(String icao, String naziv, String drzava, Lokacija lokacija) {
+        this.icao = icao;
+        this.naziv = naziv;
+        this.drzava = drzava;
+        this.lokacija = lokacija;
+    }
+
+    public Lokacija getLokacija() {
+        return lokacija;
+    }
+
+    public void setLokacija(Lokacija lokacija) {
+        this.lokacija = lokacija;
+    }
+
+    public String getIcao() {
+        return icao;
+    }
+
+    public void setIcao(String icao) {
+        this.icao = icao;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public String getDrzava() {
+        return drzava;
+    }
+
+    public void setDrzava(String drzava) {
+        this.drzava = drzava;
+    }
+    
+    
+}
